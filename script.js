3598d4b237d2b26dae04303606a738a6d5222f62
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-17 12:07:59 +0100

    add

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
-const images = [
-  "img/01.webp",
-  "img/02.webp",
-  "img/03.webp",
-  "img/04.webp",
-  "img/05.webp",
-]
+
+const {createApp} = Vue
+createApp ({
+  data(){
+
+    return{
+
+      images:[
+        
+        {
+          img: 'img/01.webp',
+          title: 'Spider-man ',
+          text: 'lorem gaerfshtwsgsy5t',
+        },
+        {
+          img: 'img/02.webp',
+          title: 'Spider-man ',
+          text: 'lorem gaerfshtwsgsy5t',
+        },
+        {
+          img: 'img/03.webp',
+          title: 'Spider-man ',
+          text: 'lorem gaerfshtwsgsy5t',
+        },
+        {
+          img: 'img/04.webp',
+          title: 'Spider-man ',
+          text: 'lorem gaerfshtwsgsy5t',
+        },
+        {
+          img: 'img/05.webp',
+          title: 'Spider-man ',
+          text: 'lorem gaerfshtwsgsy5t',
+        }
+        
+      ],
+
+      counter: 0,
+
+
+    }
+  },
+
+  methods:{
+    sliderActive(isNext){
+      
+      isNext ? this.counter++ : this.counter--;
+
+      // controllo della validita del counter
+
+      if(this.counter === this.images.length ){
+        this.counter = 0
+      }else if(this.counter < 0){
+        this.counter = this.images.length - 1;
+      }
+    }
+
+
+
+  },
+
+  mounted(){
+
+    this.sliderActive()
+
+
+
+  }
+}).mount('#app')
+
